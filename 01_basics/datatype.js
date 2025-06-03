e90d5c693a3b8d141388691725f1c59db65ae93d
pm1610 <pmm161090@gmail.com>
2025-06-03 09:23:29 +0000

    Datatypes

--- 01_basics/datatype.js
+++ 01_basics/datatype.js
@@ -1,2 +1,20 @@
-console.log("Pm");
-console.log("parth");
+"use stricts" // Treat all JS code as newer version
+
+let name = "Parth"; // string
+let age = 34 // Number
+let logedin = true // Boolean
+
+// Number
+// bigint
+// string = ""
+// Boolean ==> True or False
+// null ==> stand alone value 
+// undefined ==> not assign
+// symnol ==> unique datatype
+
+
+// Objects
+console.log(typeof "Parth");
+console.table(typeof [name,age,logedin]);
+
+// The datatype of null is object
